c0b967e219831c2316094ad9ee76226e08666fe6
AndreSalazar96 <asalazar@analiticom.com>
2019-06-10 01:21:05 -0400

    add sass and others files

---
+++ wp-content/themes/Eres Noticias/includes/no-results.php
@@ -0,0 +1,10 @@
+<div class="entry container-text-error-page">
+<!--If no results are found-->
+	<h1 class="four-cero-four"><?php esc_html_e('404','Divi'); ?></h1>
+    <p class="text-error-page"><?php esc_html_e('Oh wow, you seem a little lost, come back with us here.','Divi'); ?></p>
+    <div class="container-btn">
+        <a href="/home" class='btn-return-home'><p class="text-btn-return">Return to the page</p></a>
+    </div>
+</div>
+
+<!--End if no results are found-->
